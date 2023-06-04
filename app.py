1a717fef28d4d638cc19c240e5262f5c8ad1df62
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-03 20:09:58 -0400

    Created login and register route

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_restful import Api
+from resources.Auth import Register, Login
 
 app = Flask(__name__)
-app.run()
 
 api = Api(app)
+
+authRoutePrefix = '/auth'
+api.add_resource(Register, f'{authRoutePrefix}/register')
+api.add_resource(Login, f'{authRoutePrefix}/login')
+
+if __name__ == '__main__':
+    app.run()
--- resources/Auth.py
+++ resources/Auth.py
@@ -1,3 +1,9 @@
 from flask_restful import Resource
-from flask import request
 
+class Register(Resource):
+    def post(self):
+        return 'Registering', 201
+    
+class Login(Resource):
+    def post(self):
+        return 'Logining', 201
