f20c5bc76bce690cb7ef6522799cd6dc03827118
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-04 02:20:38 -0400

    Started work on user model

---
+++ models/user.py
@@ -0,0 +1,11 @@
+from models.db import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.now())
