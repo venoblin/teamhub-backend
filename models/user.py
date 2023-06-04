6aaf6fec83d7d6efae2ea0603653d3cfc2f05377
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-04 02:27:18 -0400

    Migrated changes

--- models/user.py
+++ models/user.py
@@ -9,3 +9,8 @@ class User(db.Model):
     password = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.now())
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
