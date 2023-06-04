ff7e6408f6e2755fd934237d0dec79596cdfd704
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-04 19:36:59 -0400

    Added get and post endpoints for users

---
+++ resources/user.py
@@ -0,0 +1,18 @@
+from flask_restful import Resource
+from flask import request
+from models.db import db
+from models.user import User
+
+class Users(Resource):
+  
+  def get(self):
+    data = User.find_all()
+    results = [u.json() for u in data]
+    return results
+  
+  def post(self):
+    data = request.get_json()
+    user = User(**data)
+    user.create()
+    return user.json(), 201
+
