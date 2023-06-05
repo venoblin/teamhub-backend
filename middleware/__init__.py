385fb85444ec73b2bf4cde382961c8bff8ee7646
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-04 21:12:27 -0400

    Added read token function

--- middleware/__init__.py
+++ middleware/__init__.py
@@ -9,3 +9,12 @@ SECRET_KEY = os.getenv('SECRET')
 
 def create_token(payload):
     return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+def read_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return payload
+    except jwt.InvalidSignatureError:
+        return 'Signature invalid'
+    except jwt.InvalidTokenError:
+        return 'Token invalid'
