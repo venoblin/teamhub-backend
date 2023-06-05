7bf3daaca88f29205c3333e8067e59eae480e5db
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-04 21:16:58 -0400

    Added password and strip token functions

--- middleware/__init__.py
+++ middleware/__init__.py
@@ -18,3 +18,16 @@ def read_token(token):
         return 'Signature invalid'
     except jwt.InvalidTokenError:
         return 'Token invalid'
+    
+def hash_password(password):
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+def check_password(password, hashed_password):
+    return bcrypt.checkpw(password.encode(), hashed_password.encode())
+
+def strip_token(req):
+    try:
+        token = req.headers['authorization'].split(' ')[1]
+        return token
+    except:
+        return None
