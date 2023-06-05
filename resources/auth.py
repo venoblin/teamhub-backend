556acd450ee3f4f68cfd6c27909f601bd092b088
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-05 19:57:14 -0400

    Added registering

--- resources/auth.py
+++ resources/auth.py
@@ -1,9 +1,20 @@
 from flask_restful import Resource
+from flask import request
 from middleware import create_token, hash_password, strip_token, read_token, check_password
+from models.user import User
 
 class Register(Resource):
     def post(self):
-        return 'Registering', 201
+        data = request.get_json()
+        params = {
+            "username": data['username'],
+            "name": data['name'],
+            "email": data['email'],
+            "password": hash_password(data['password'])
+        }
+        user = User(**params)
+        user.create()
+        return user.json(), 201
     
 class Login(Resource):
     def post(self):
