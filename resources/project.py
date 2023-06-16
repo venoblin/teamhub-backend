8b923cbba3757d55ba88736e059f00b6e26403f4
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-06-15 22:01:25 -0400

    Created project resource

---
+++ resources/project.py
@@ -0,0 +1,9 @@
+from flask_restful import Resource
+from models.project import Project
+
+class Projects(Resource):
+    def get():
+      pass
+        
+    def post():
+      pass
--- resources/user.py
+++ resources/user.py
@@ -1,19 +1,20 @@
 from flask_restful import Resource
-from flask import request
-from models.db import db
 from models.user import User
 
 class Users(Resource):
+  @classmethod
   def get(self):
     data = User.find_all()
     results = [u.json() for u in data]
     return results
 
 class SingleUser(Resource):
+  @classmethod
   def get(self, id):
     data = User.find_by_id(id)
     return data.json()
   
+  @classmethod
   def delete(self, id):
     data = User.delete_by_id(id)
     return data
