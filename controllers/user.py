1fa3380aa52b9d4ca92e9a5ae223060a5738b947
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-02 22:57:48 -0400

    added get single user  controller

--- controllers/user.py
+++ controllers/user.py
@@ -1,6 +1,20 @@
+from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import subqueryload
 from models.user import User
 
 def get_all_users():
   data = User.find_all()
   results = [u.json() for u in data]
   return results
+
+def get_single_user(id):
+  user = User.query.options(subqueryload(User.projects)).filter_by(id=id).first()
+
+  def construct_project(project):
+    todos = [t.json() for t in project.todos]
+    bugs = [b.json() for b in project.bugs]
+    return {**project.json(), 'todos': todos, 'bugs': bugs}
+    
+  projects = [construct_project(p) for p in user.projects]
+
+  return {**user.json(), 'projects': projects}
--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,4 @@
 from flask_restful import Resource
-from sqlalchemy.orm import joinedload
-from sqlalchemy.orm import subqueryload
-from models.user import User
 
 class Users(Resource):
   def get(self):
@@ -9,14 +6,5 @@ class Users(Resource):
 
 class SingleUser(Resource):
   def get(self, id):
-    user = User.query.options(subqueryload(User.projects)).filter_by(id=id).first()
-
-    def construct_project(project):
-      todos = [t.json() for t in project.todos]
-      bugs = [b.json() for b in project.bugs]
-      return {**project.json(), 'todos': todos, 'bugs': bugs}
-      
-    projects = [construct_project(p) for p in user.projects]
-
-    return {**user.json(), 'projects': projects}
+    pass
     
