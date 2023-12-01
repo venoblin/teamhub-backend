ca25b63fcb4f295da9ecee8a23fb57e2a0548856
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-30 20:57:36 -0500

    finished post controller

--- controllers/event.py
+++ controllers/event.py
@@ -5,3 +5,12 @@ def get_all_events():
   data = Event.find_all()
   results = [e.json() for e in data]
   return results
+
+def post_event():
+  data = request.json()
+  params = {
+    'event': data['event']
+  }
+  event = Event(**params)
+  event.create()
+  return event.json(), 201
