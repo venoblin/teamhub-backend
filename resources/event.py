e69648705b89b60ca44a159f4b3fa7ffd2c5a5e9
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-11-30 21:03:19 -0500

    finished resources

--- resources/event.py
+++ resources/event.py
@@ -1,4 +1,18 @@
 from flask_restful import Resource
 from flask import request
 from middleware import verify_auth
+from controllers.event import get_all_events, post_event, get_single_event, delete_single_event
 
+class Events(Resource):
+  def get(self):
+    return verify_auth(request, get_all_events)
+  
+  def post(self):
+    return verify_auth(request, post_event)
+
+class SingleEvent(Resource):
+  def get(self, id):
+    return verify_auth(request, lambda: get_single_event(id))
+  
+  def delete(self, id):
+    return verify_auth(request, lambda: delete_single_event(id))
