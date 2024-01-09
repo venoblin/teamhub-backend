c37182fd25345f2b3918971c10af8ff71981cecb
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-01-08 19:38:28 -0500

    remove event resources

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from resources.user import Users, SingleUser
 from resources.project import Projects, SingleProject
 from resources.todo import Todos, SingleTodo
 from resources.bug import Bugs, SingleBug
-from resources.event import Events, SingleEvent
 
 app = Flask(__name__)
 CORS(app)
@@ -46,9 +45,6 @@ api.add_resource(SingleTodo, '/todos/<int:id>')
 
 api.add_resource(Bugs, '/bugs')
 api.add_resource(SingleBug, '/bugs/<int:id>')
-
-api.add_resource(Events, '/events')
-api.add_resource(SingleEvent, '/events/<int:id>')
 
 if __name__ == '__main__':
     app.run()
