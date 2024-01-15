b91aae4428bc5ffc4120364ac542046a31e00809
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-01-14 23:17:57 -0500

    finished resources

--- resources/notification.py
+++ resources/notification.py
@@ -1,3 +1,18 @@
 from flask_restful import Resource
 from flask import request
 from middleware import handle_request
+from controllers.notification import get_all_notifications, post_notification, get_single_notification, delete_single_notification
+
+class Notifications(Resource):
+  def get(self):
+    return handle_request(request, get_all_notifications)
+  
+  def post(self):
+    return handle_request(request, post_notification)
+
+class SingleNotification(Resource):
+  def get(self, id):
+    return handle_request(request, lambda: get_single_notification(id))
+  
+  def delete(self, id):
+    return handle_request(request, lambda: delete_single_notification(id))
